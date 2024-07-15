8e89c7c5af9ef594fedda2d08f8f80ba1d368710
Jonathan Baird <jon@northrankdigital.com>
2024-07-14 20:37:20 -0500

    Moved uninstall code from Inc\Base\deactivate.php to uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -7,7 +7,32 @@
  */
 
 if (!defined('WP_UNINSTALL_PLUGIN')) {
-  die;
+  $custom_post_types = [];
+
+
+  foreach ($custom_post_types as $post_type) {
+    $posts = get_posts([
+      'post_type' => $post_type,
+      'numberposts' => -1,
+      'post_status' => 'any'
+    ]);
+
+    foreach ($posts as $post) {
+      wp_delete_posts($post->ID, true);
+    }
+  }
+
+  $options = [
+    'nrd_untappd_importer_schedule',
+    'nrd_untappd_importer_api_creds',
+    'nrd_untappd_importer_untappd_menu',
+    'nrd_untappd_importer_menu',
+    'nrd_untappd_importer'
+  ];
+
+  foreach ($options as $option) {
+    delete_option($option);
+  }
 }
 
 // TODO: Clear Database stored data
