ea4051758bd82cc1b0f2ff8dbe4063a7d5610ae7
Jonathan Baird <jon@northrankdigital.com>
2024-07-16 02:00:37 -0500

    cosmetic fixes

--- templates/partials/support.php
+++ templates/partials/support.php
@@ -2,6 +2,5 @@
   <div>
     <p>Questions? <b>Email: <a href="mailto:support@northrankdigital.com">support@northrankdigital.com</a></b></p>
     <p><i><b>Disclaimer:</b> North Rank Digital is not associated in any way to Untappd. This plugin is third party software.</i></p>
-    <p>v1.0.1</p>
   </div>
 </div>
